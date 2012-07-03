c4f4ff8ba018484adddec764bfa85b950ff514b7
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-07-03 09:11:46 -0400

    dynamic file upload

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -4,16 +4,17 @@ class Upload < ActiveRecord::Base
 
   def self.upload_to_s3(file, title, artist)
 	   upload = Upload.new(title: title, artist: artist)
-     FileUploader.store("#{artist}-#{title}", add_meta_data(file), CURRENT_BUCKET, access: :public_read)
-     upload.url = "http://#{CURRENT_BUCKET}.s3.amazonaws.com/#{artist}-#{title}"
+     file_name = "#{artist}-#{title}"
+     FileUploader.store("#{file_name}", add_meta_data(file, title, artist), CURRENT_BUCKET, access: :public_read)
+     upload.url = "http://#{CURRENT_BUCKET}.s3.amazonaws.com/#{file_name}"
      upload.save
   end
 
-  def self.add_meta_data(file_url)
-  	file = open("http://ec-media.soundcloud.com/7mbrzwW0QvaH.128.mp3?ff61182e3c2ecefa438cd02102d0e385713f0c1faf3b0339595667fb0d0ded1d4001494890461e840ba2a7cfbabdcdaab9719c7655dd543c85aee62b59b443f8c507831559&AWSAccessKeyId=AKIAJBHW5FB4ERKUQUOQ&Expires=1341286936&Signature=YTeyoG%2FRhkPnCehdwubGP1UcpwA%3D")
+  def self.add_meta_data(file_url, title, artist)
+  	file = open(file_url)
 		Mp3Info.open(file.path) do |mp3|
-		   mp3.tag.title = "track title"
-		   mp3.tag.artist = "artist name"
+		   mp3.tag.title  = title
+		   mp3.tag.artist = artist
 		end
 		file
   end
