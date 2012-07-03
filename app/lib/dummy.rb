d74b92f5e776ab5855309a59466a8720ebe3afe1
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-07-03 15:53:10 -0400

    updating reciever

--- app/lib/dummy.rb
+++ app/lib/dummy.rb
@@ -1,22 +1,13 @@
-require 'net/http'
-require 'uri'
-
+require 'faraday'
+require 'awesome_print'
 class Awesome
 
   def self.fetch(uri_str, limit = 10)
-    # You should choose better exception.
-    raise ArgumentError, 'HTTP redirect too deep' if limit == 0
-    url = URI.parse(uri_str)
-    req = Net::HTTP::Get.new(url.path)
-    response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
-    case response
-    when Net::HTTPSuccess     then response
-    when Net::HTTPRedirection then fetch(response['location'], limit - 1)
-    else
-      response.error!
+    Faraday.get "http://hypem.com/serve/play/1n8bs/e5c78fae6292e1ce00ebf16244b5a603" do |req|
+      req.headers["Cookie"] = "AUTH=03%3Aad986f5e59838895cd867706aad40c13%3A1341336508%3A1117341873%3ADC-US"
     end
   end
 
 end
 
-# print Awesome.fetch('http://www.ruby-lang.org')
+ap Awesome.fetch('http://www.ruby-lang.org').headers["location"]
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,14 @@
 require 'open-uri'
 class Upload < ActiveRecord::Base
   attr_accessible :url, :artist, :title
+  AUTH = "03%3Aad986f5e59838895cd867706aad40c13%3A1341336508%3A1117341873%3ADC-US"
+
+  def self.catch_redirect(uri_str)
+    resp = Faraday.get uri_str do |req|
+            req.headers["Cookie"] = "AUTH=#{AUTH}"
+           end
+    resp.headers["location"]
+  end
 
   def self.upload_to_s3(file, title, artist)
 	   upload = Upload.new(title: title, artist: artist)
@@ -11,7 +19,7 @@ class Upload < ActiveRecord::Base
   end
 
   def self.add_meta_data(file_url, title, artist)
-  	file = open(file_url)
+  	file = open(catch_redirect(file_url))
 		Mp3Info.open(file.path) do |mp3|
 		   mp3.tag.title  = title
 		   mp3.tag.artist = artist
