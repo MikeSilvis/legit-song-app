33240579f7513d5922b4bfb47baadb07c8f364a5
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-07-04 12:56:30 -0400

    rescue for shitty songs

--- app/controllers/uploaders_controller.rb
+++ app/controllers/uploaders_controller.rb
@@ -7,11 +7,11 @@ class UploadersController < ApplicationController
 
   def create
     params[:tracks].each do |track|
-      Thread.new do
+      #Thread.new do
         track  = track[1]
         file   = "http://hypem.com/serve/play/#{track['id']}/#{track['key']}"
         upload = Upload.upload_to_s3(file, track["song"], track["artist"], track["id"], track["thumb_url_large"])
-      end
+      #end
     end
     render json: "MIKE YOUR AWESOME", status: :created
   end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -5,7 +5,7 @@ class Upload < ActiveRecord::Base
   validates_uniqueness_of :song_id
   AUTH = "03%3Aa93d72140f864529e3c350aaf5231bf2%3A1334250701%3A1144071075%3ADC-US"
 
-  def self.catch_redirect(uri_str)
+  def catch_redirect(uri_str)
     resp = Faraday.get uri_str do |req|
             req.headers["Cookie"] = "AUTH=#{AUTH}"
            end
@@ -15,16 +15,21 @@ class Upload < ActiveRecord::Base
   def self.upload_to_s3(file, title, artist, song_id, artwork)
      file_name = "#{artist}-#{title}.mp3"
 	   upload = Upload.new(title: title, artist: artist, song_id: song_id, url: "http://#{CURRENT_BUCKET}.s3.amazonaws.com/#{file_name}", artwork: artwork)
+     upload.add_meta_data(file,title, artist)
      FileUploader.store("#{file_name}", upload.add_meta_data(file, title, artist), CURRENT_BUCKET, access: :public_read) if upload.save
   end
 
   def add_meta_data(file_url, title, artist)
-  	file = open(catch_redirect(file_url))
-		Mp3Info.open(file.path) do |mp3|
-		   mp3.tag.title  = title
-		   mp3.tag.artist = artist
-		end
-		self.errors.add(:file, "FILE WON't UPLOAD") unless file
+    begin
+    	file = open(catch_redirect(file_url))
+  		Mp3Info.open(file.path) do |mp3|
+  		   mp3.tag.title  = title
+  		   mp3.tag.artist = artist
+  		end
+    rescue
+      puts "shit... #{title} - #{artist}"
+    end
+      file
   end
 
 end
