93e3300648c007aebbfc2e0d13e3e9b90722266e
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-07-04 08:54:23 -0400

    threads!

--- app/controllers/uploaders_controller.rb
+++ app/controllers/uploaders_controller.rb
@@ -7,9 +7,11 @@ class UploadersController < ApplicationController
 
   def create
     params[:tracks].each do |track|
-      track  = track[1]
-      file   = "http://hypem.com/serve/play/#{track['id']}/#{track['key']}"
-      upload = Upload.upload_to_s3(file, track["song"], track["artist"], track["id"], track["thumb_url_large"])
+      Thread.new do
+        track  = track[1]
+        file   = "http://hypem.com/serve/play/#{track['id']}/#{track['key']}"
+        upload = Upload.upload_to_s3(file, track["song"], track["artist"], track["id"], track["thumb_url_large"])
+      end
     end
     render json: "MIKE YOUR AWESOME", status: :created
   end
