bfc83007bd4907cd853f986e58f793e221f81807
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-07-04 12:59:46 -0400

    removing route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 FileUpload::Application.routes.draw do
   resource :uploader
-
-  #CORS
-  match '/uploader', :controller => 'uploaders', :action => 'options', :constraints => {:method => 'OPTIONS'}
 end
